fd64ae2faea8b1f135d02fef84bef7d863a72dc5
sdylewski <sdylewski@daac0390-c61a-0410-8102-d06dd8142015>
2011-02-19 17:07:50 +0000

    updated dir2slideshow with -nofades option
    git-svn-id: https://svn.code.sf.net/p/dvd-slideshow/code@108 daac0390-c61a-0410-8102-d06dd8142015

--- examples/cleanup_files.sh
+++ examples/cleanup_files.sh
@@ -82,17 +82,6 @@ rm -f -r dvd-menu_temp*
 rm -f -r my_slideshow_pics*
 cd ..
 ######################################
-cd simple
-rm -f *.xml *.wav *.flv
-rm -f *.mpg ; rm -f *.vob
-rm -f *.log
-rm -f *.cue ; rm -f *.bin
-rm -f -r dvd_fs
-rm -f -r dvd-slideshow_temp*
-rm -f -r dvd-menu_temp*
-rm -f *.iso
-cd ..
-######################################
 cd cool
 rm -f *.xml *.wav *.flv
 rm -f *.mpg ; rm -f *.vob
--- examples/create_dvd_from_all_examples.sh
+++ examples/create_dvd_from_all_examples.sh
@@ -6,6 +6,41 @@ else
         rm -rf dvd_fs/*
 fi
 
+if [ ! -e 'very_simple/verysimple.vob' ] ; then
+	cd very_simple
+	nice sh runtest.sh
+	cd ..
+fi
+if [ ! -e 'simple/simple.vob' ] ; then
+	cd simple
+	nice sh runtest.sh
+	cd ..
+fi
+if [ ! -e 'audio/audio.vob' ] ; then
+	cd audio
+	nice sh runtest.sh
+	cd ..
+fi
+if [ ! -e 'complex/complex.vob' ] ; then
+	cd complex
+	nice sh runtest.sh
+	cd ..
+fi
+if [ ! -e 'complete/complete.vob' ] ; then
+	cd complete
+	nice sh runtest.sh
+fi
+if [ ! -e '0.7.5/0.7.5.vob' ] ; then
+	cd '0.7.5'
+	nice sh runtest.sh
+	cd ..
+fi
+if [ ! -e '0.8.0/0.8.0.vob' ] ; then
+	cd '0.8.0'
+	nice sh runtest.sh
+	cd ..
+fi
+
 dvd-menu -t 'Very Simple' -t 'Simple' -t 'Audio' -t 'Complex' -t 'Complete' -t '0.7.5' -t '0.8.0' -f very_simple/verysimple.xml -f simple/simple.xml -f audio/audio.xml -f complex/complex.xml -f 'complete/test complete.xml' -f '0.7.5/my_slideshow.xml' -f '0.8.0/my_slideshow.xml' -n 'Example DVD' -iso
 
 #xine dvd:"`pwd`/dvd_fs/"  &
